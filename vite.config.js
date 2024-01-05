0f96fc3b7ddd2fbb8bc4e1eb153d7d00f0842f02
ShaneChen0219 <hsiangyuan0219@gmail.com>
2024-01-05 12:39:18 -0700

    deploy repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-deploy/",
+  base:"/3DPortfolio/",
   // config for include glb file
   assetsInclude:['**/*.glb']
 })
